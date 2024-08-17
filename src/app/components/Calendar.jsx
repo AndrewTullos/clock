2a7af6b657b49b802acb7b69bc1d0a44eae18a6a
Drew <tullos.andrew@gmail.com>
2024-08-17 13:42:40 -0500

    Clock

---
+++ src/app/components/Calendar.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+
+function Calendar() {
+	return <div>Calendar</div>;
+}
+
+export default Calendar;
+
+// import * as React from "react";
+
+// import { Calendar } from "@/components/ui/calendar";
+
+// export function CalendarDemo() {
+// 	const [date, setDate] = (React.useState < Date) | (undefined > new Date());
+
+// 	return (
+// 		<Calendar
+// 			mode="single"
+// 			selected={date}
+// 			onSelect={setDate}
+// 			className="rounded-md border"
+// 		/>
+// 	);
+// }
--- src/app/components/Clock.jsx
+++ src/app/components/Clock.jsx
@@ -1,11 +1,20 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 function Clock() {
 	const time = new Date().toLocaleTimeString();
+	const [currentTime, setCurrentTime] = useState(time);
+
+	const updateTime = () => {
+		const time = new Date().toLocaleTimeString();
+		setCurrentTime(time);
+	};
+
+	setInterval(updateTime, 1000);
+
 	return (
 		<div>
-			<h1>{time}</h1>
+			<h1>{currentTime}</h1>
 		</div>
 	);
 }
